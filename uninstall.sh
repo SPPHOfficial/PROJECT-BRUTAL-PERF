4dbf4b4081c859c3500f8458e1b553486c7a215a
sirancheta <73354897+sirancheta@users.noreply.github.com>
2021-09-02 11:51:56 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,34 +1,6 @@
-# Don't modify anything after this
-
-if [ -f $INFO ]; then
-
-  while read LINE; do
-
-    if [ "$(echo -n $LINE | tail -c 1)" == "~" ]; then
-
-      continue
-
-    elif [ -f "$LINE~" ]; then
-
-      mv -f $LINE~ $LINE
-
-    else
-
-      rm -f $LINE
-
-      while true; do
-
-        LINE=$(dirname $LINE)
-
-        [ "$(ls -A $LINE 2>/dev/null)" ] && break 1 || rm -rf $LINE
-
-      done
-
-    fi
-
-  done < $INFO
-
-  rm -f $INFO
-
-fi
-
+id=PROJECT_BRUTAL_PERF
+name=PROJECT BRUTAL PERF
+version=Final Build
+versionCode=3383
+author=SIRANCHETA Creation | SPPH TEAM
+description=New way to boost your Gaming experience | To see full installation visit https://katb.in/kat11584
