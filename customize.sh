329cddd06f0838a9a4864ad5b589b0d3c073c64d
Sir Ancheta <73354897+sirancheta@users.noreply.github.com>
2021-09-12 21:22:49 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,4 +1,19 @@
-#!/sbin/sh
-SKIPUNZIP=0
-REPLACE=""
+REPLACE_EXAMPLE="
+/system/app/Youtube
+/system/priv-app/SystemUI
+/system/priv-app/Settings
+/system/framework
+"
 
+REPLACE="
+"
+
+set_permissions() {
+  set_perm_recursive $MODPATH/system/vendor/etc/permissions 0 0 0755 0644
+  set_perm_recursive $MODPATH/system/vendor/lib 0 0 0755 0644
+  set_perm_recursive $MODPATH/system/vendor/lib/egl 0 0 0755 0644
+  set_perm_recursive $MODPATH/system/vendor/lib/hw 0 0 0755 0644
+  set_perm_recursive $MODPATH/system/vendor/lib64 0 0 0755 0644
+  set_perm_recursive $MODPATH/system/vendor/lib64/egl 0 0 0755 0644
+  set_perm_recursive $MODPATH/system/vendor/lib64/hw 0 0 0755 0644
+}
